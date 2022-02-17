e1822756c24c0cd2c42c4aaef1e8b6e2083e614e
Zanco1993 <zanconatofabio@gmail.com>
2022-02-17 17:08:45 +0100

    milestone 1 complete

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+include __DIR__ . "/db/db.php";
 
 ?>
 
@@ -13,13 +14,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-ajax-dischi</title>
     <link rel="stylesheet" href="./style/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 
 <body>
-    <header>
-        <img src="./img/logo.png" alt="">
-    </header>
+    <div id="app">
 
+        <header>
+            <img src="./img/logo.png" alt="">
+        </header>
+
+        <div class="container-cards">
+            <!-- ciclo le card con php -->
+            <?php foreach ($dischi['response'] as $disc) {
+            ?>
+                <div class="box-card">
+                    <img src="<?php echo $disc['poster']?>" alt="">
+                    <h3><?php echo $disc['title'] ?></h3>
+                    <p><?php echo $disc['author'] ?></p>
+                    <p><?php echo $disc['year']?></p>
+                </div>
+                
+                <?php } ?>
+
+        </div>
+
+
+
+    </div>
+
+
+    <script src="./js/script.js"></script>
 </body>
 
 </html>
